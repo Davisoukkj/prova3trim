732a6e0ef1217ff10cd49545f9dd59dab80d49da
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2023-10-23 08:26:27 -0300

    fiz oq deu

---
+++ prova3trim/java.js
@@ -0,0 +1,25 @@
+function Calcular(){
+    let calculadora = document.querySelector("index.html")
+    
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    let idadeHumana = idadeCanina * 7
+
+    if (idadeHumana <= 11){
+        idade = "Seu pet é criança";
+    }
+    if (idadeHumana <= 17){
+        idade = "Seu pet é adolescente";
+    }
+    if (idadeHumana <= 29){
+        idade = "Seu pet é adulto";
+    }
+    if (idadeHumana <= 60){
+        idade = "Seu pet é idoso";
+    }
+
+    qualResultado.textContent = "Resultado: A idade humana do cachorrinho é " idadeHumana + idadeCanina" anos. Ele é um "idade".";{
+    }
+    
+    //BRO ESTOU PERDIDO NÃO TA FAZENDO SOL, VOCÊ PODE ME AJUDAR A TERMINAR ESSA CALCULADORA PARA MEU FILHO??
+    //POR FAVOR BRO TERMINE PARA MIM GANG GANG GANG 777 FERNANDX CLOATHING
+}
